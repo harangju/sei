0b9188674fd65ff3c05a06a6b46ca63fe70d40c0
Harang Ju <harangju@icloud.com>
2022-01-07 13:53:17 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 import sei
 import discord
-import dislash
 import configparser
 from loguru import logger
 
@@ -13,29 +12,26 @@ except:
 
 intents = discord.Intents(messages=True, guilds=True)
 client = discord.Client(intents=intents)
-slash = dislash.InteractionClient(client)
 
-bot = discord.Client(intent=intents)
-slash = 
+try:
+    prefix = config.get('Bot', 'prefix')
+    if prefix=='':
+        raise Exception('Missing prefix in config.cfg')
+except:
+    logger.error('Please enter `prefix = &` in [Bot] in config.cfg.')
+    exit()
 
-# try:
-#     prefix = config.get('Bot', 'prefix')
-#     if prefix=='': raise Exception('Missing prefix in config.cfg')
-# except:
-#     logger.error('Please enter `prefix = &` in [Bot] in config.cfg.')
-#     exit()
-#
-# @client.event
-# async def on_ready():
-#     logger.info('We have logged in as {0.user}'.format(client))
-#
-# @client.event
-# async def on_message(message):
-#     if message.author == client.user: # message from bot
-#         return
-#     if not message.content.startswith(prefix):
-#         return
-#     await bot.router.route(message)
+@client.event
+async def on_ready():
+    logger.info('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user: # message from bot
+        return
+    if not message.content.startswith(prefix):
+        return
+    await sei.router.route(message)
 
 try:
     token = config.get('Discord', 'token')
